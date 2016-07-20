1806c3a77bc300f231398e16d4a52526c9c2a454
Lance Johnson <lancejjohnson@gmail.com>
2016-07-20 07:32:01 -0400

    Progress on the regex examples

--- app/regex.js
+++ app/regex.js
@@ -2,23 +2,23 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.regexAnswers = {
   containsNumber: function(str) {
-
+    return (/\d+/).test(str);
   },
 
   containsRepeatingLetter: function(str) {
-
   },
 
   endsWithVowel: function(str) {
-
+    return (/[aeiou]$/i).test(str);
   },
 
   captureThreeNumbers: function(str) {
-
+    var capture = (/(\d{3})/).exec(str);
+    return capture ? capture[0] : false;
   },
 
   matchesPattern: function(str) {
-
+    return (/^\d{3}-\d{3}-\d{4}$/).test(str);
   },
 
   isUSD: function(str) {
