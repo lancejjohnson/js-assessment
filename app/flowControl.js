c634523fbff73b935600c0c1c966c486b86e320c
Lance Johnson <lancejjohnson@gmail.com>
2016-09-25 07:06:44 -0400

    Completes flowControl problem

--- app/flowControl.js
+++ app/flowControl.js
@@ -1,15 +1,27 @@
 exports = typeof window === 'undefined' ? global : window;
 
+function divisibleBy(dividend, divisor) {
+  return dividend % divisor === 0;
+}
+
 exports.flowControlAnswers = {
   fizzBuzz: function(num) {
-    // write a function that receives a number as its argument;
-    // if the number is divisible by 3, the function should return 'fizz';
-    // if the number is divisible by 5, the function should return 'buzz';
-    // if the number is divisible by 3 and 5, the function should return
-    // 'fizzbuzz';
-    //
-    // otherwise the function should return the number, or false if no number
-    // was provided or the value provided is not a number
+    if (typeof num !== 'number') {
+      return false;
+    }
 
+    var answer;
+
+    if (divisibleBy(num, 3) && divisibleBy(num, 5)) {
+      answer = 'fizzbuzz';
+    } else if (divisibleBy(num, 3)) {
+      answer = 'fizz';
+    } else if (divisibleBy(num, 5)) {
+      answer = 'buzz';
+    } else {
+      answer = num;
+    }
+
+    return answer;
   }
 };
